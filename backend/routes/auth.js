1f5c1fb909bca6db060d0dd1807895baf4bc9702
ROHIT KUMAR <74416087+dynamiccoder-gif@users.noreply.github.com>
2024-04-06 15:46:11 +0530

    use jwt for token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,9 +2,11 @@ import express from 'express';
 import User from '../models/User.js';
 import bodyParser from 'body-parser';
 import { body, validationResult } from 'express-validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
 const router = express.Router();
 router.use(bodyParser.json());
-
+const jwtSecret = 'your_strong_secret_key'; 
 
 router.post('/', [
   body('username').isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
@@ -18,10 +20,14 @@ router.post('/', [
   }
 
   try {
-    const user = new User(req.body);
+    const hashedPassword = await bcrypt.hash(req.body.password, 10); // Hash password with bcrypt
+    const user = new User({ ...req.body, password: hashedPassword });
     await user.save(); // Use async/await for clarity
-    console.log(`User created successfully: ${user.username}`);
-    res.send({ message: 'User created successfully' }); // Informative response
+   
+    
+    const token = jwt.sign({userId: user._id }, jwtSecret, { expiresIn: '1h' }); // Generate JWT
+    res.status(201).json({ message: 'User created successfully', token }); // Send JWT in response
+
   } catch (error) {
     console.error(error); // Log actual error
     res.status(500).json({ message: 'Account already exists' }); // Generic error message for user
